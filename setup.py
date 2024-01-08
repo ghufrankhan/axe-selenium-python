1df97826b14ffd94b044a2d122d2aa5822c3a4ef
ghufran <29932028+ghufrankhan@users.noreply.github.com>
2024-01-08 15:19:15 +0000

    Update axe 482 (#7)
    * Update README.rst
    
    * Update setup.py
    
    * Update package.json
    
    * Update package-lock.json

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("./README.rst") as f:
 
 setup(
     name="axe-selenium-python-nhsuk",
-    version="1.0.2",
+    version="1.0.3",
     description="Python library to integrate axe and selenium for web \
                 accessibility testing.",
     long_description=open("README.rst").read(),
